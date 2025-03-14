0aed5c2e2ad9eb24633600e8e6128253f6f6731f
fady <fady41487@gmail.com>
2025-03-14 03:00:07 +0200

    Implementation of Employee BLL

---
+++ Company.G01.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,52 @@
+﻿using Company.G01.BLL.Interfaces;
+using Company.G02.DAL.Data.Contexts;
+using Company.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G01.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly CompanyDBContext _context;
+        public EmployeeRepository(CompanyDBContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+
+
+        public Employee? Get(int id)
+        {
+            return _context.Employees.Find(id);
+        }
+
+        public int Add(Employee model)
+        {
+            _context.Employees.Add(model);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Employee model)
+        {
+            _context.Employees.Update(model);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Employee model)
+        {
+            _context.Employees.Remove(model);
+            return _context.SaveChanges();
+        }
+
+
+
+    }
+}
